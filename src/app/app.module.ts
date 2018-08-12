55c121b3e4fc47e97196152de635f08bcf9beb9e
Ricardo Camacho <racamachogomez@gmail.com>
2018-08-12 11:27:01 -0400

    add table with that retrive data from html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,20 @@
 
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { MyNavComponent } from './my-nav/my-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
+import { RouterModule, Routes } from '@angular/router';
+import { NXIDashboardComponent } from './nxi-dashboard/nxi-dashboard.component';
+import { NXIDevelopersComponent } from './nxi-developers/nxi-developers.component';
+import { NXIProjectsComponent } from './nxi-projects/nxi-projects.component';
 
+//
 import '../polyfills';
 
 import {CdkTableModule} from '@angular/cdk/table';
 import {CdkTreeModule} from '@angular/cdk/tree';
 import {HttpClientModule} from '@angular/common/http';
-
+import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {BrowserModule} from '@angular/platform-browser';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-/*import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule,
-MatTable, MatColumnDef,MatHeaderCell, MatHeaderCellDef, MatCell, MatCellDef,
-MatHeaderRowDef, MatRow, MatRowDef, 
-MatTableDataSource, MatFormField } from '@angular/material';*/
 import {
   MatAutocompleteModule,
   MatBadgeModule,
@@ -58,18 +52,60 @@ import {
   MatTooltipModule,
   MatTreeModule,
 } from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-
-
-import { NXIDashboardComponent } from './nxi-dashboard/nxi-dashboard.component';
-import { NXIDevelopersComponent } from './nxi-developers/nxi-developers.component';
-import { NXIProjectsComponent } from './nxi-projects/nxi-projects.component';
-
+//
 const appRoutes: Routes = [
   { path: 'nxi-dashboard', component: NXIDashboardComponent},
   { path: 'nxi-developers', component: NXIDevelopersComponent},
   { path: 'nxi-projects', component: NXIProjectsComponent}
 ];
+
+@NgModule({
+  exports: [
+    CdkTableModule,
+    CdkTreeModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+  ]
+})
+export class DemoMaterialModule {}
+
 
 @NgModule({
   declarations: [
@@ -78,61 +114,19 @@ const appRoutes: Routes = [
     NXIDashboardComponent,
     NXIDevelopersComponent,
     NXIProjectsComponent,
-   
-
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes),
     LayoutModule,
-     // Material components
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatCardModule,
-   
-      CdkTableModule,
-      CdkTreeModule,
-      MatAutocompleteModule,
-      MatBadgeModule,
-      MatBottomSheetModule,
-      MatButtonModule,
-      MatButtonToggleModule,
-      MatCardModule,
-      MatCheckboxModule,
-      MatChipsModule,
-      MatStepperModule,
-      MatDatepickerModule,
-      MatDialogModule,
-      MatDividerModule,
-      MatExpansionModule,
-      MatGridListModule,
-      MatIconModule,
-      MatInputModule,
-      MatListModule,
-      MatMenuModule,
-      MatNativeDateModule,
-      MatPaginatorModule,
-      MatProgressBarModule,
-      MatProgressSpinnerModule,
-      MatRadioModule,
-      MatRippleModule,
-      MatSelectModule,
-      MatSidenavModule,
-      MatSliderModule,
-      MatSlideToggleModule,
-      MatSnackBarModule,
-      MatSortModule,
-      MatTableModule,
-      MatTabsModule,
-      MatToolbarModule,
-      MatTooltipModule,
-      MatTreeModule,
-    
-    
+
+
+    FormsModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
